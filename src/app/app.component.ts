124bbc58a44d6dd75b4b10e56720576a32cdd0a2
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-15 14:14:49 -0600

    rename app root element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'spotify-clone-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
