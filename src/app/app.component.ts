cbde6fad0175a918c23ea768bccb12db472200f4
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-15 15:10:58 -0600

    add interceptors for api routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+/// <reference types="spotify-api" />
 
+import { Component, OnInit } from '@angular/core';
+import { AuthStore } from './core/auth.store';
 @Component({
-  selector: 'spotify-clone-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'spotify-clone-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'spotify-clone';
+export class AppComponent implements OnInit {
+    title = 'spotify-clone';
+
+    constructor(private authStore: AuthStore) {}
+
+    ngOnInit() {
+        this.authStore.init();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { unauthorizedInterceptorProvider } from './core/interceptors/unauthorized.interceptor';
+import { authInterceptorProvider } from './core/interceptors/auth.interceptor';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [authInterceptorProvider, unauthorizedInterceptorProvider],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
