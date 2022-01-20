a8e96cbee29fb515d8097f20883c76cc8e6cf90a
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-20 11:52:57 -0600

    add navigation functions to buttons

--- src/app/core/components/topbar/topbar.component.ts
+++ src/app/core/components/topbar/topbar.component.ts
@@ -1,15 +1,21 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'spotify-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+    selector: 'spotify-topbar',
+    templateUrl: './topbar.component.html',
+    styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent implements OnInit {
+    constructor(private location: Location) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    goBack() {
+        this.location.back();
+    }
 
+    goForward() {
+        this.location.forward();
+    }
 }
