9fac7fb5a096820ea1684e3623b903083037c254
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-21 10:28:31 -0600

    implement search page features

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SearchApiService } from 'src/app/core/services/search.service';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+    public queryParams: any;
+    public searchResults: any = null;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private search: SearchApiService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.route.queryParams.subscribe((res) => {
+            console.log(res);
+            this.queryParams = res;
+            if (this.queryParams.searchQuery) {
+                this.getSearchResults(this.queryParams.searchQuery);
+            }
+        });
+    }
 
+    getSearchResults(searchQuery: string) {
+        this.search.search(searchQuery).subscribe((res) => {
+            this.searchResults = res;
+            console.log(res);
+        });
+    }
+
+    getCardSubtitle(subtitle: string) {
+        return `By ${subtitle}`;
+    }
 }
--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
 import { TopbarModule } from 'src/app/core/components/topbar/topbar.module';
+import { MusicCardModule } from 'src/app/core/components/music-card/music-card.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { TopbarModule } from 'src/app/core/components/topbar/topbar.module';
   imports: [
     CommonModule,
     SearchRoutingModule,
-    TopbarModule
+    TopbarModule,
+    MusicCardModule
   ]
 })
 export class SearchModule { }
