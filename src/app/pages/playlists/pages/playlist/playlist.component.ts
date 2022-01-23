ec0c306ab637654639658bee216a421d01d1a0c7
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-23 11:09:22 -0600

    add background color section to page, fix routing issue

--- src/app/pages/playlists/pages/playlist/playlist.component.ts
+++ src/app/pages/playlists/pages/playlist/playlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ActivationEnd, Params, Router } from '@angular/router';
+import { ColorService } from 'src/app/core/services/color.service';
 import { PlaylistService } from 'src/app/core/services/playlist.service';
 
 @Component({
@@ -11,24 +12,46 @@ export class PlaylistComponent implements OnInit {
     public playlistId: string = '';
     public playlistInfo: any = null;
     public playlistTracks: any = null;
-    constructor(private router: Router, private route: ActivatedRoute, private playlistService: PlaylistService) {
-      this.playlistId = this.route.snapshot.paramMap.get('playlistId') || '';
+    public backgroundColor: string = '#009688';
+    public blockId: string | number = '';
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private playlistService: PlaylistService,
+        private colors: ColorService
+    ) {
+        this.route.paramMap.subscribe((res: Params) => {
+            this.playlistInfo = null;
+            this.playlistTracks = null;
+            console.log(res['params'].playlistId);
+            this.playlistId = res['params'].playlistId;
+            this.getPlaylistById();
+            this.getPlaylistTracks();
+        });
     }
 
     ngOnInit(): void {
-      this.getPlaylistById();
-      this.getPlaylistTracks();
+        
     }
 
     getPlaylistById() {
-      this.playlistService.getById(this.playlistId).subscribe((res) => {
-        this.playlistInfo = res;
-      })
+        this.playlistService.getById(this.playlistId).subscribe((res) => {
+            this.playlistInfo = res;
+            this.getAlbumColors(this.playlistInfo.images[0].url);
+        });
     }
 
     getPlaylistTracks() {
-      this.playlistService.getTracks(this.playlistId).subscribe((res) => {
-        this.playlistTracks = res;
-      })
+        this.playlistService.getTracks(this.playlistId).subscribe((res) => {
+            this.playlistTracks = res;
+        });
+    }
+
+    getAlbumColors(url: string) {
+        this.colors.getAlbumColors(url).subscribe((res) => {
+            // console.log(url, playlistIndex);
+            this.playlistInfo.colors = res;
+            this.backgroundColor = res[0];
+        });
     }
 }
