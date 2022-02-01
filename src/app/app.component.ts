7932e1bffaf47c4d3a700a5a6879b097d8eb685e
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-31 20:10:58 -0600

    register svg icons and playback service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,35 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { AuthStore } from './core/auth.store';
+import { AuthStore } from './core/stores/auth.store';
+import { PlaybackService } from './core/services/playback.service';
 @Component({
     selector: 'spotify-clone-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
     title = 'spotify-clone';
 
-    constructor(private authStore: AuthStore, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        iconRegistry.addSvgIconLiteral('play', sanitizer.bypassSecurityTrustHtml('assets/icons/play.svg'));
-        iconRegistry.addSvgIconLiteral('previous', sanitizer.bypassSecurityTrustHtml('assets/icons/previous.svg'));
-        iconRegistry.addSvgIconLiteral('next', sanitizer.bypassSecurityTrustHtml('assets/icons/next.svg'));
+    constructor(
+        private authStore: AuthStore,
+        private playbackService: PlaybackService,
+        iconRegistry: MatIconRegistry,
+        sanitizer: DomSanitizer
+    ) {
+        iconRegistry.addSvgIcon('play', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/play.svg'));
+        iconRegistry.addSvgIcon('pause', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/pause.svg'));
+        iconRegistry.addSvgIcon('previous', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/previous.svg'));
+        iconRegistry.addSvgIcon('next', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/next.svg'));
+        iconRegistry.addSvgIcon('shuffle', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/shuffle.svg'));
+        iconRegistry.addSvgIcon('repeat', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/repeat.svg'));
+        iconRegistry.addSvgIcon('home', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/home.svg'));
+        iconRegistry.addSvgIcon('library', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/library.svg'));
+        iconRegistry.addSvgIcon('search', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/search.svg'));
     }
 
     ngOnInit() {
         this.authStore.init();
+        this.playbackService.init();
     }
 }
