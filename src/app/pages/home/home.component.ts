19f274af5a7ccf1d246a3ba903f7fc85356ff047
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-31 20:12:31 -0600

    get albums colors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -43,8 +43,12 @@ export class HomeComponent implements OnInit {
 
     getAlbumColors(url: string, playlistIndex: number) {
         this.colors.getAlbumColors(url).subscribe((res) => {
+            const brightColors = res.filter((res) => {
+                return this.getBrighterColor(res);
+            });
+            console.log(brightColors);
             // console.log(url, playlistIndex);
-            this.featuredPlaylists.playlists.items[playlistIndex].colors = res;
+            this.featuredPlaylists.playlists.items[playlistIndex].colors = brightColors;
         });
     }
 
@@ -54,5 +58,20 @@ export class HomeComponent implements OnInit {
         } else {
             this.backgroundColor = this.featuredPlaylists.playlists.items[0].colors[0];
         }
+    }
+
+    getBrighterColor(hex: string) {
+        var c = hex.substring(1); // strip #
+        var rgb = parseInt(c, 16); // convert rrggbb to decimal
+        var r = (rgb >> 16) & 0xff; // extract red
+        var g = (rgb >> 8) & 0xff; // extract green
+        var b = (rgb >> 0) & 0xff; // extract blue
+
+        var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+        if (luma > 140) {
+            return true;
+        }
+        return false;
     }
 }
